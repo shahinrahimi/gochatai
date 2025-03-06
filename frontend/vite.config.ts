19f0a25a3b189acb2411ed70051b72b14f999f1b
Shahin Rahimi <sh.rahimi.dev@gmail.com>
2025-03-06 14:36:38 +0330

    init shadcn, downgrading to react18

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,18 @@
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: 3000,
-  }
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  //server: {
+  //  host: '0.0.0.0',
+  //  port: 3000,
+  //}
 })
