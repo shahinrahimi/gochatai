65f1b5407ac11566c45a0ffc90936640611114ad
Shahin Rahimi <sh.rahimi.dev@gmail.com>
2025-03-06 17:13:55 +0330

    setup basic cors

--- backend/cmd/api/routes.go
+++ backend/cmd/api/routes.go
@@ -5,10 +5,14 @@ import (
   "backend/internal/middlewares"
 
 	"github.com/gofiber/fiber/v2"
+  "github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 
 func SetupRoutes(app *fiber.App) {
+  app.Use(cors.New(cors.Config{
+    AllowOrigins: "http://localhost:5173",
+  }))
   // register global middlewares
   app.Use(middlewares.Logger)
 
