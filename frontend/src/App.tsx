ece9dde836ee53fdd8f3e79566af82cd94cd0253
Shahin Rahimi <sh.rahimi.dev@gmail.com>
2025-03-06 14:46:23 +0330

    add simple button for testing shadcn

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,15 @@
 import './App.css'
-
+import { Button } from './components/ui/button'
 function App() {
 
-  return (<>Hi this is not simple as you may think</>)
+  return (
+    <div className="bg-red-100">
+      Hi this is not simple as you may think
+      but it's simple as death
+      added a new component with shadcn
+      <Button>Click Me</Button>
+    </div>
+  )
 
 }
 export default App
