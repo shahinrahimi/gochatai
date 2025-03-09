9bff3745de7fc4ffdfdd717eb7e6d6e56d7f7416
Shahin Rahimi <sh.rahimi.dev@gmail.com>
2025-03-09 22:17:51 +0330

    refactor stream function

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -2,7 +2,7 @@ import { Model } from '../types/'
 const API_BASE_URL = "http://localhost:3000"
 const API_LIST_MODELS = API_BASE_URL + "/list"
 const API_GENERATE_ONCE = API_BASE_URL + "/generate"
-//const API_GENERATE_STREAM = API_BASE_URL + "/generatestream"
+const API_GENERATE_STREAM = API_BASE_URL + "/generatestream"
 
     
 
@@ -51,3 +51,51 @@ export const fetchReplyOnce = async (modelName: string, prompt: string): Promise
     return ""
   }
 }
+
+export const fetchReplyStream = async (modelName: string, prompt: string, onResponse: (text: string) => void, onError: (error:any) => void) => {
+  const url = API_GENERATE_STREAM
+  try {
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({
+        model: modelName,
+        prompt: prompt
+      })
+    })
+
+    const reader = res.body?.getReader()
+    if (!reader) throw new Error("Failed to get reader from response body")
+    const decoder = new TextDecoder()
+    let done = false
+    while (!done) {
+      const { value, done: doneReading } = await reader.read()
+      if (doneReading) break
+      const chunk = decoder.decode(value, { stream: true })
+      const jsonObject = chunk.trim().split("\n")
+      jsonObject.forEach((element) => {
+        try {
+          const data = JSON.parse(element)
+          if (data.response) {
+            const text = data.response
+            // Handling thinking state
+            if (text.includes("<think>")) {
+              //isThinking = true;
+            } else if (text.includes("</think>")) {
+              //isThinking = false;
+            } else {
+              onResponse(text);
+            }
+          }
+        } catch (error) {
+          console.error("Failed to parse JSON error: ", error)
+          onError(error)
+        }
+      })
+    }
+  } catch (error) {
+    console.error("Failed to fetching stram: ", error)
+    onError(error)
+  }
+}
+
