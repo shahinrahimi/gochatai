5a22b2fde44a323ab4fd14dea0bdefbba89c742b
Shahin Rahimi <sh.rahimi.dev@gmail.com>
2025-03-19 19:37:14 +0330

    update

--- backend/cmd/api/handlers.go
+++ backend/cmd/api/handlers.go
@@ -1,18 +1,16 @@
 package main
 
 import (
-	"bytes"
 	"context"
 	"encoding/json"
 	"errors"
 	"fmt"
-	"io"
 	"net/http"
 
 	"github.com/shahinrahimi/ollamalite/ollama"
 )
 
-func(app *Application) Generate(w http.ResponseWriter, r*http.Request) error {
+func(app *Application) GenerateCompletion(w http.ResponseWriter, r*http.Request) error {
   // parse requeset to struct
   var oReq ollama.GenerateCompletionReq 
   if err := ReadJSON(w,r,&oReq); err != nil {
@@ -34,7 +32,7 @@ func(app *Application) Generate(w http.ResponseWriter, r*http.Request) error {
   })
 }
 
-func (app *Application)GenerateChat(w http.ResponseWriter, r*http.Request) error {
+func (app *Application)GenerateChatCompletion(w http.ResponseWriter, r*http.Request) error {
   var or ollama.GenerateChatCompletionReq
   if err := ReadJSON(w, r, &or); err != nil {
     return ErrorJSON(w, err)
@@ -52,7 +50,7 @@ func (app *Application)GenerateChat(w http.ResponseWriter, r*http.Request) error
   })
 }
 
-func (app *Application) GenerateCompletionStream (w http.ResponseWriter, r*http.Request) error {
+func (app *Application) GenerateCompletionSSE (w http.ResponseWriter, r*http.Request) error {
   ctx := r.Context()
   w.Header().Set("Content-Type", "json/application")
   w.Header().Set("Cashe-Control", "no-cache")
@@ -82,104 +80,63 @@ func (app *Application) GenerateCompletionStream (w http.ResponseWriter, r*http.
 
 
 }
-
-func GenerateStreamEnabled(w http.ResponseWriter, r*http.Request) error {
-  var or OllamaGenerateRequestPayload
-  if err := ReadJSON(w, r, &or); err != nil {
-    return ErrorJSON(w, err)
-  }
-  
-  // temp: make sure it is streamed
-  or.Stream = true
-
-  // create http objest
-  requestURL := fmt.Sprintf("%s:%s", OllamaBaseUrl, "/api/generate")
-  jsonData, err := json.Marshal(or)
-  if err != nil {
-    return ErrorJSON(w, err)
-  }
-  req, err := http.NewRequest(http.MethodPost,requestURL,bytes.NewBuffer(jsonData))
-  if err != nil {
-    return ErrorJSON(w, err)
-  }
-  // set proper header
-  req.Header.Set("Content-Type", "application/json")
-
-  // creating the client
-  cl := http.Client{}
-  resp, err := cl.Do(req)
-  if err != nil {
-    return ErrorJSON(w, err)
-  }
-  defer resp.Body.Close()
-  
-
-  // set the necessary headers for streaming
-  w.Header().Set("Content-Type", "application/json")
+func (app *Application) GenerateChatCompletionSSE (w http.ResponseWriter, r*http.Request) error {
+  ctx := r.Context()
+  w.Header().Set("Content-Type", "json/application")
+  w.Header().Set("Cashe-Control", "no-cache")
+  w.Header().Set("Connection", "keep-alive")
   w.Header().Set("Transfer-Encoding", "chunked")
+  flusher, ok := w.(http.Flusher)
+  if !ok{
+    return ErrorJSON(w, errors.New("flusher is not ok!"))
+  }
+  var oReq ollama.GenerateChatCompletionReq
+  if err := ReadJSON(w, r, &oReq); err != nil {
+    return ErrorJSON(w,err)
+  }
 
-  // stream data from ollama to the user
-  buffer := make([]byte, 1024)
+  outCh, errCh := app.oc.GenerateChatCompletionSSE(ctx, oReq)
   for {
-    n, err := resp.Body.Read(buffer)
-    if err != nil {
-      // TODO check end of stream - or simple error
-      fmt.Printf("err in reading buffer of the body err: %v", err)
-      break
-    }
-
-    // Write the chunk to the response
-    if n > 0 {
-      _, err := w.Write(buffer[:n])
-      if err != nil {
-        fmt.Printf("err in writing to response: %v", err)
-        break
-      }
-
-      // Ensure that the chunk is sent immediately to the clinet
-      // this is type assertion
-      // http.ResponseWriter also implements the http.Flusher
-      w.(http.Flusher).Flush()
+    select {
+    case chunk, _ := <-outCh:
+      data, _ := json.Marshal(chunk)
+      fmt.Fprintln(w, string(data))
+      flusher.Flush()
+    
+    case err, _ := <-errCh:
+      return ErrorJSON(w, fmt.Errorf("Streaming error: %w", err))
     }
   }
-  
- return nil 
+
 
 }
 
-func ListModels(w http.ResponseWriter, r*http.Request) error {
-  // create http object
-  urlRequest := fmt.Sprintf("%s:%s", OllamaBaseUrl, "/api/tags")
-  req, err := http.NewRequest(http.MethodGet, urlRequest, nil)
-  if err != nil {
-    return ErrorJSON(w, err)
-  }
 
-  req.Header.Set("Content-Type", "application/json")
-
-  // create http client
-  cl := http.Client{}
-  resp, err := cl.Do(req)
+func(app *Application) ListLocalModels(w http.ResponseWriter, r*http.Request) error {
+  resp, err := app.oc.ListLocalModels(context.TODO())
   if err != nil {
-    return ErrorJSON(w, err)
-  }
-  defer resp.Body.Close()
-
-  body, err := io.ReadAll(resp.Body)
-  if err != nil {
-    return ErrorJSON(w, err)
-  }
-  
-  var ol OllamaListModelsResponsePayload
-  if err := json.Unmarshal(body, &ol); err != nil {
     return ErrorJSON(w, err)
   }
 
   return WriteJSON(w, http.StatusOK, &jsonResponse{
     Error: false,
-    Message: "successfuly query model list",
-    Data: ol,
-  })
+    Message: "successfully query local models",
+    Data: resp,
+  })  
 }
+func(app *Application) ListRunningModels(w http.ResponseWriter, r*http.Request) error {
+  resp, err := app.oc.ListRunningModels(context.TODO())
+  if err != nil {
+    return ErrorJSON(w, err)
+  }
+
+  return WriteJSON(w, http.StatusOK, &jsonResponse{
+    Error: false,
+    Message: "successfully query running models",
+    Data: resp,
+  })  
+}
+
+
 
 
--- backend/cmd/api/types.go
+++ backend/cmd/api/types.go
@@ -1,82 +1,82 @@
 package main
 
-const (
-  OllamaBaseUrl = "http://host.docker.internal:11434" // access localhost:11434 in docker
-)
-
-// Generate a completion
-// POST => /api/generete
-type OllamaGenerateRequestPayload struct {
-  Model string `json:"model"`    
-  Prompt string `json:"prompt"`   
-  Stream bool `json:"stream"` 
-  Suffix string `json:"suffix,omitempty"`
-  Image []string `json:"image,omitempty"` 
-  Format string `json:"format,omitempty"` 
-  System string `json:"system,omitempty"` // 
-  Raw bool `json:"raw,omitempty"` // default false
-  Options OllamaGenerateRequestOptions `json:"options,omitempty"`
-  KeepAlive string `json:"keep_alive,omitempty"` // default 5m 
-}
-
-type OllamaGenerateRequestOptions struct {
-  Seed int64 `json:"seed,emitempty"`
-  Temperature float64 `json:"temperature,omitempty"`
-  NumCtx int `json:"num_ctx,omitempty"` // Sets the size of the context window used to generate the next token default 2048
-}
-
-type OllamaGenerateResponsePayload struct {
-  Model string `json:"model"`
-  Response string `json:"response"`
-  Done bool `json:"done"` 
-  PromptEvalCount int64  `json:"prompt_eval_count"`
-  PromptEvalDuration int64 `json:"prompt_eval_duration"`
-  EvalCount int64 `json:"eval_count"`
-  EvalDuration int64 `json:"eval_duration"`
-  TotalDuration int64 `json:"total_duration"`
-  LoadDuration int64 `json:"load_duration"`
-}
-
-// Generate a chat
-// POST => /api/chat
-type OllamaGenerateChatRequest struct {
-  Model string `json:"model"`
-  Messages []OllamaMessageChat `json:"messages"`
-  Tools []string `json:"tools"` 
-  Format string `json:"format"`
-  Stream bool  `json:"stream"`
-  KeepAlive string `json:"keep_alive"`
-}
-
-type OllamaMessageChat struct {
-  Role string `json:"role"`
-  Content string `json:"content"`
-  Image []string `json:"image"`
-  ToolCalls []string `json:"tool_calls"`
-}
-
-
-// List Models
-// GET => /api/models
-type OllamaModelDetails struct {
-  Format string `json:"format"`
-  Family string `json:"family"`
-  Families *string `json:"famieies"` // handle null references
-  ParamaterSize string `json:"paramater_size"`
-  QuantizationLevel string `json:"quantization_level"`
-}
-type OllamaModelInfo struct {
-  Name string `json:"name"`
-  ModifiedAt string `json:"modified_at"`
-  Size int64 `json:"size"`
-  Digest string `json:"digest"` 
-  Details OllamaModelDetails `json:"details"`
-}
-type OllamaListModelsResponsePayload struct {
-  Models []OllamaModelInfo `json:"models"`
-}
-
-
-
-
-    
+// const (
+//   OllamaBaseUrl = "http://host.docker.internal:11434" // access localhost:11434 in docker
+// )
+//
+// // Generate a completion
+// // POST => /api/generete
+// type OllamaGenerateRequestPayload struct {
+//   Model string `json:"model"`    
+//   Prompt string `json:"prompt"`   
+//   Stream bool `json:"stream"` 
+//   Suffix string `json:"suffix,omitempty"`
+//   Image []string `json:"image,omitempty"` 
+//   Format string `json:"format,omitempty"` 
+//   System string `json:"system,omitempty"` // 
+//   Raw bool `json:"raw,omitempty"` // default false
+//   Options OllamaGenerateRequestOptions `json:"options,omitempty"`
+//   KeepAlive string `json:"keep_alive,omitempty"` // default 5m 
+// }
+//
+// type OllamaGenerateRequestOptions struct {
+//   Seed int64 `json:"seed,emitempty"`
+//   Temperature float64 `json:"temperature,omitempty"`
+//   NumCtx int `json:"num_ctx,omitempty"` // Sets the size of the context window used to generate the next token default 2048
+// }
+//
+// type OllamaGenerateResponsePayload struct {
+//   Model string `json:"model"`
+//   Response string `json:"response"`
+//   Done bool `json:"done"` 
+//   PromptEvalCount int64  `json:"prompt_eval_count"`
+//   PromptEvalDuration int64 `json:"prompt_eval_duration"`
+//   EvalCount int64 `json:"eval_count"`
+//   EvalDuration int64 `json:"eval_duration"`
+//   TotalDuration int64 `json:"total_duration"`
+//   LoadDuration int64 `json:"load_duration"`
+// }
+//
+// // Generate a chat
+// // POST => /api/chat
+// type OllamaGenerateChatRequest struct {
+//   Model string `json:"model"`
+//   Messages []OllamaMessageChat `json:"messages"`
+//   Tools []string `json:"tools"` 
+//   Format string `json:"format"`
+//   Stream bool  `json:"stream"`
+//   KeepAlive string `json:"keep_alive"`
+// }
+//
+// type OllamaMessageChat struct {
+//   Role string `json:"role"`
+//   Content string `json:"content"`
+//   Image []string `json:"image"`
+//   ToolCalls []string `json:"tool_calls"`
+// }
+//
+//
+// // List Models
+// // GET => /api/models
+// type OllamaModelDetails struct {
+//   Format string `json:"format"`
+//   Family string `json:"family"`
+//   Families *string `json:"famieies"` // handle null references
+//   ParamaterSize string `json:"paramater_size"`
+//   QuantizationLevel string `json:"quantization_level"`
+// }
+// type OllamaModelInfo struct {
+//   Name string `json:"name"`
+//   ModifiedAt string `json:"modified_at"`
+//   Size int64 `json:"size"`
+//   Digest string `json:"digest"` 
+//   Details OllamaModelDetails `json:"details"`
+// }
+// type OllamaListModelsResponsePayload struct {
+//   Models []OllamaModelInfo `json:"models"`
+// }
+//
+//
+//
+//
+//
