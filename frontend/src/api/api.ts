05c21f369ff165fc38e26ef0f534b08466828906
Shahin Rahimi <sh.rahimi.dev@gmail.com>
2025-03-22 19:46:33 +0330

    add middlware to api

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,8 +1,8 @@
 
 import {RequestMiddleware, ResponseMiddleware} from "@/api/types"
+import { RequestLogger, ResponseLogger } from "./middlewares";
+
 const API_BASE_URL = "http://localhost:8000";
-
-
 
 class ApiClient {
   private baseUrl:string;
@@ -114,87 +114,11 @@ class ApiClient {
 const apiClient = new ApiClient(API_BASE_URL);
 
 // Request Middlewares
-apiClient.onRequest(async(request, url) => {
-  console.log("🔵 [API Request]", {url, method: request.method, headers:request.headers, body: JSON.stringify(request.body, null,2)})
-  if (request.body && typeof request.body == "string") {
-    console.log("body:", JSON.stringify(JSON.parse(request.body), null, 2))
-  }
-})
+apiClient.onRequest(RequestLogger)
 
 // Response Middlewares
-apiClient.onResponse(async (response, url) => {
-  console.log("🟢 [API Response]", { url, status: response.status })
-  return response;
-})
+apiClient.onResponse(ResponseLogger)
 
 export default apiClient;
 
-//export async function api<T>(
-//  endpoint: string,
-//  method: "GET" | "POST",
-//  options: RequestInit = {},
-//  body?: any,
-//  onSuccess?: (data: T) => void,
-//  onError?: (error: any) => void
-//):Promise<T | void> {
-//  const url = API_BASE_URL + endpoint  
-//  try {
-//    const response = await fetch(url, {
-//      method,
-//      headers: {
-//        ...options.headers, // allow additional headers
-//        //...getHeaders(),
-//      },
-//      body: body ? JSON.stringify(body) : undefined,
-//    })
-//
-//    if (!response.ok){
-//      throw new Error(`HTTP error! Status: ${response.status}`);
-//    }
-//    const data: T = await response.json();
-//    console.log(`Response fron ${url}: `, data);
-//
-//    if (onSuccess) onSuccess(data);
-//    return data;
-//  }catch(error){
-//    console.error("API Error: ", error);
-//    if (onError) onError(error);
-//  }
-//}
-//
-//
-//export async function apiStreem(
-//  endpoint: string,
-//  method: "GET" | "POST",
-//  options: RequestInit = {},
-//  body?:any,
-//  onChunk?:(chunk: string) => void,
-//  onError?: (error:any) => void
-//):Promise<void> {
-//  const url = API_BASE_URL + endpoint
-//  try{
-//    const response = await fetch(url, {
-//      method,
-//      headers: {
-//        ...options.headers,
-//        //...getHeaders()
-//      },
-//      body: body ? JSON.stringify(body): undefined
-//    })
-//
-//    const reader = response.body?.getReader();
-//    if (!reader) throw new Error("Failed to get reader from response body")
-//
-//      const decoder = new TextDecoder();
-//      let done = false;
-//      while (!done) {
-//        const { value, done: doneReading} = await reader.read();
-//        if (doneReading) break;
-//        const chunk = decoder.decode(value, { stream: true });
-//        if (onChunk) onChunk(chunk)
-//      }
-//  }catch(error){
-//    console.error("Streaming API Error: ", error);
-//    if (onError) onError(error)
-//  }
-//}
+
